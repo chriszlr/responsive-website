71b7f8ea1f895154b8cf9919cf002a9f52f2b3da
Christopher Zöllner <chriszoelln@gmail.com>
2023-04-01 10:10:12 +0200

    readme + fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,9 +29,7 @@ const Navbar = () => {
             : "ease-in-out duration-500 fixed left-[-100%]"
         }
       >
-        <h1 className="w-full  text-3xl font-bold text-[#00df9a] m-4">
-          REACT.
-        </h1>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
         <ul className="uppercase p-4">
           <li className="p-4 border-b border-gray-600">Home</li>
           <li className="p-4 border-b border-gray-600">Company</li>
